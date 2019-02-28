da44af85b7545b7d376671a03244653deea9c753
Dhara Patel <1991dharapatel@gmail.com>
2019-02-28 03:15:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ function nowServing(line){
  if (line.length===0) {return "There is nobody waiting to be served!"}
  else {return `Currently serving ${line.shift()}.`}
 }
+
+function currentLine(line){
+  
+  return `The line is currently: ${name}, `
+}
