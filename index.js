9c0dfd686edeeddae5b7afaa5bc3cef366ea8675
Dhara Patel <1991dharapatel@gmail.com>
2019-02-28 03:51:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ function currentLine(line){
   if (line.length===0) {return "The line is currently empty."}
   else
   {for (var i=0; i<line.length; i++){s = s + (i+1)+'. '+line[i]+', '}
-  return s.slice(0,s.length-1)}
+  return s.slice(0,s.length-2)}
 }
