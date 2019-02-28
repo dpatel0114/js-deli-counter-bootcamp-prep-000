e526a63a7e4d93a965a53fe0d88f351e1848fb29
Dhara Patel <1991dharapatel@gmail.com>
2019-02-28 03:51:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function currentLine(line){
   var s="The line is currently:"
   if (line.length===0) {return "The line is currently empty."}
   else
-  {for (var i=0; i<line.length; i++){s = s + (i+1)+'. '+line[i]+','}
+  {for (var i=0; i<line.length; i++){s = s + (i+1)+'. '+line[i]+', '}
   return s.slice(0,s.length-1)}
 }
